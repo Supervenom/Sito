4eb6ffb40c97d7b6c63ce7e640a126aa4a942d1f
Gabriele <vanonigabriele@gmail.com>
2014-01-06 21:03:44 +0100

    Updates

--- dropzone/upload.php
+++ dropzone/upload.php
@@ -1,17 +1,41 @@
 <?php
+include 'ChromePhp.php';
+$ds          = DIRECTORY_SEPARATOR; 
+$space = ' ';
  
-$upload_folder = 'file-upload';
+$storeFolder = 'file-upload';  
+ 
 if (!empty($_FILES)) {
-  $temp_file = $_FILES['file']['tmp_name'];
-  $target_path = dirname( __FILE__ ) .  '/' . $upload_folder . '/';
-  $target_file =  $target_path . $_FILES['file']['name'];
+ 
+    $tempFile = $_FILES['file']['tmp_name'];         
+ 
+    $targetPath = dirname( __FILE__ ) . $ds. $storeFolder . $ds; 
+ 
+    $targetFile =  $targetPath. $_FILES['file']['name']; 
+ 
+    move_uploaded_file($tempFile,$targetFile);
+	
+	$command = 'lp -d _192_168_001_048 '. $targetFile;
+	ChromePhp::log($command);
+	
+	$output = shell_exec($command);
+ 
+} else {                                                           
+    $result  = array();
+ 
+    $files = scandir($storeFolder);                 //1
+    if ( false!==$files ) {
+        foreach ( $files as $file ) {
+            if ( '.'!=$file && '..'!=$file) {       //2
+                $obj['name'] = $file;
+                $obj['size'] = filesize($storeFolder.$ds.$file);
+                $result[] = $obj;
+            }
+        }
+    }
      
- 
-  if( file_exists( $target_path ) ) {
-    move_uploaded_file($temp_file, $target_file);
-  } else {
-    header($_SERVER['SERVER_PROTOCOL'] . ' 500 Internal Server Error', true, 500);
-  }
+    header('Content-type: text/json');              //3
+    header('Content-type: application/json');
+    echo json_encode($result);
 }
- 
 ?>
