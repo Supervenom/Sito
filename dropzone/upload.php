b88066602a90a8d9632b46da0ab17bedfb8dcd9e
Gabriele <vanonigabriele@gmail.com>
2014-01-06 17:10:25 +0100

    Update PHP

---
+++ dropzone/upload.php
@@ -0,0 +1,17 @@
+<?php
+ 
+$upload_folder = 'uploads';
+if (!empty($_FILES)) {
+  $temp_file = $_FILES['file']['tmp_name'];
+  $target_path = dirname( __FILE__ ) .  '/' . $upload_folder . '/';
+  $target_file =  $target_path . $_FILES['file']['name'];
+     
+ 
+  if( file_exists( $target_path ) ) {
+    move_uploaded_file($temp_file, $target_file);
+  } else {
+    header($_SERVER['SERVER_PROTOCOL'] . ' 500 Internal Server Error', true, 500);
+  }
+}
+ 
+?>
